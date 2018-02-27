4a329c7eea1f6a56ef85831f6c420913fd63206b
Kristijonas Urbaitis <kristijonas.u@gmail.com>
2018-02-27 18:21:33 +0200

    Signalzen destroy event

--- assets/signalzen.js
+++ assets/signalzen.js
@@ -10,19 +10,8 @@ var SignalZen = (function() {
 
       host = location.host;
       if (host.split(".").length === 1) {
-        // no "." in a domain - it's localhost or something similar
         document.cookie = name + "=" + value + expires + "; path=/";
       } else {
-        // Remember the cookie on all subdomains.
-        //
-        // Start with trying to set cookie to the top domain.
-        // (example: if user is on foo.com, try to set
-        //  cookie to domain ".com")
-        //
-        // If the cookie will not be set, it means ".com"
-        // is a top level domain and we need to
-        // set the cookie to ".foo.com"
-
         document.cookie =
           name + "=" + value + expires + "; path=/; domain=" + domain;
       }
@@ -112,6 +101,7 @@ var SignalZen = (function() {
 
         if (data.event == 'deleteCookie') {
           Cookie.erase(data.name, data.domain);
+          self.postMessage({ event: 'deleteCookie', name: data.name, identifier: data.identifier });
         }
 
         if (data.event == 'getCookie') {
